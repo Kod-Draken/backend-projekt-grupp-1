06f8d4a5318a4c3c22772f15c9726c5f8d227fb3
AlrikM <alrbri0804@skola.goteborg.se>
2025-05-21 12:25:37 +0200

    GymClass Interface

--- src/main/java/se/yrgo/services/GymClassManagementService.java
+++ src/main/java/se/yrgo/services/GymClassManagementService.java
@@ -7,7 +7,15 @@ import java.util.List;
 public interface GymClassManagementService {
 
     public GymClass newGymClass(GymClass newClass);
-    public GymClass changeGymClass(GymClass changeClassName);
+
+    public GymClass getGymClassWithName(String gymClassName);
+
+    public GymClass getGymClassWithId(int gymClassId);
+
+    public GymClass changeGymClass(GymClass changedClass);
+
     public GymClass deleteGymClass(GymClass gymClassName);
+
     public List<GymClass> getAllGymClasses();
+
 }

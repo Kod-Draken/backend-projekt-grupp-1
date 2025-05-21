37fecd72259f20de77a3ae674e37c70db9f78ca2
Najib Bardash <najib.bardash@gmail.com>
2025-05-21 13:43:28 +0200

    Adding code for instructor

--- src/main/java/se/yrgo/domain/Instructor.java
+++ src/main/java/se/yrgo/domain/Instructor.java
@@ -1,5 +1,47 @@
 package se.yrgo.domain;
 
+import javax.persistence.*;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
 public class Instructor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(length = 20)
+    private String instructorId;
+
+    @Column(length = 20)
+    private String name;
+
+    @Column(length = 20)
+    private String phone;
+
+    private int numberOfClasses;
+
+    @OneToMany
+    @JoinColumn(name = "INSTRUCTOR_FK")
+    private Set<GymClass> gymClasses;
+
+    public Instructor() {}
+
+    public Instructor(String instructorId, String name, String phone) {
+        this.instructorId = instructorId;
+        this.name = name;
+        this.phone = phone;
+        this.gymClasses = new HashSet<>();
+    }
+
+    public void addGymClassToInstructorSchedule(GymClass gymclass) {
+        this.gymClasses.add(gymclass);
+    }
+
+    public Set<GymClass> getInstructorGymClasses() {
+        return Collections.unmodifiableSet(this.gymClasses);
+    }
 
 }

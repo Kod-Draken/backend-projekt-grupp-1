9dd2d42577fb7dec4eebdd216b0735ca13143cbf
AlrikM <alrbri0804@skola.goteborg.se>
2025-05-21 15:08:25 +0200

    Updated MemberDao, MemberMangmntImpl

--- src/main/java/se/yrgo/dataaccess/MemberDao.java
+++ src/main/java/se/yrgo/dataaccess/MemberDao.java
@@ -5,17 +5,17 @@ import se.yrgo.domain.Member;
 import java.util.List;
 
 public interface MemberDao {
-    public void Create(Member member);
+    public void create(Member member);
 
-    public void Update(Member updateMember);
+    public void update(Member updateMember);
 
-    public void Delete(Member deleteMember);
+    public void delete(Member deleteMember);
 
     public Member getById(String id) throws Exception;
 
-    public List<Member> getAllMembers();
+    public List<Member> getAll();
 
     public List<Member> getByName(String name);
 
-    public Member getFullMemberDetail(String memberId);
+    public Member getFullDetail(String memberId);
 }
--- src/main/java/se/yrgo/services/MemberManagementProdImpl.java
+++ src/main/java/se/yrgo/services/MemberManagementProdImpl.java
@@ -1,5 +1,40 @@
 package se.yrgo.services;
 
-public class MemberManagementProdImpl {
+import se.yrgo.domain.Member;
+import se.yrgo.dataaccess.MemberDao;
+import java.util.List;
 
+public class MemberManagementProdImpl implements MemberManagementService{
+    private final MemberDao memDao;
+    public MemberManagementProdImpl(MemberDao dao) {
+        this.memDao = dao;
+    }
+
+    public void newMember(Member newMember){
+        memDao.create(newMember);
+    }
+
+    public void editMember(Member changedMember){
+        memDao.update(changedMember);
+    }
+
+    public void deleteMember(Member deletedMember){
+        memDao.delete(deletedMember);
+    }
+
+    public Member findMemberById(String id) throws Exception{
+        return memDao.getById(id);
+    }
+
+    public List<Member> getAllMembers(){
+        return memDao.getAll();
+    }
+
+    public List<Member> getMembersByName(String name){
+        return memDao.getByName(name);
+    }
+
+    public Member getFullMemberDetail(String memberId){
+        return memDao.getFullDetail(memberId);
+    }
 }

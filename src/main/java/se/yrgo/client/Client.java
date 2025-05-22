75c23fd53dcd905502328a15ed8fb67f1800c7d9
Kod-Draken <m.dackhammar@gmail.com>
2025-05-22 16:21:15 +0200

    added some more stuff in setUpData in Client.java

--- src/main/java/se/yrgo/client/Client.java
+++ src/main/java/se/yrgo/client/Client.java
@@ -1,8 +1,14 @@
 package se.yrgo.client;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import se.yrgo.domain.GymClass;
 import se.yrgo.domain.Instructor;
+import se.yrgo.domain.Member;
+import se.yrgo.services.GymClassManagementService;
 import se.yrgo.services.InstructorManagementService;
+import se.yrgo.services.MemberManagementService;
+
+import java.time.LocalDateTime;
 
 /**
  * This class is the Client in an application where you can book and manage gym classes
@@ -27,8 +33,13 @@ public class Client {
     private static void setUpData() {
         container = new ClassPathXmlApplicationContext("production-application.xml");
         InstructorManagementService is = container.getBean(InstructorManagementService.class);
+        GymClassManagementService gm = container.getBean(GymClassManagementService.class);
+        MemberManagementService mm = container.getBean(MemberManagementService.class);
 
         is.newInstructor(new Instructor("IN1", "Bosse Bredsladd", "031-777444"));
+        gm.addNewGymClass(new GymClass("Boxning", "Lätt boxningspass fokus på hög puls och förbättrad koordination", "Sal 3", LocalDateTime.now(), 20));
+        mm.newMember(new Member("S001","Janne Björnsson","0758293153"));
+
 
         container.close();
     }

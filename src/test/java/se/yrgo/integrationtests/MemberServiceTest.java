d2240feea5d1d2bfabcee131b12c38c7921b017d
AlrikM <alrbri0804@skola.goteborg.se>
2025-05-23 15:00:10 +0200

    Fixed warnings

--- src/test/java/se/yrgo/integrationtests/MemberServiceTest.java
+++ src/test/java/se/yrgo/integrationtests/MemberServiceTest.java
@@ -6,13 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
-import se.yrgo.domain.GymClass;
-import se.yrgo.domain.Instructor;
 import se.yrgo.domain.Member;
 import se.yrgo.services.exceptions.MemberIdNotFoundException;
 import se.yrgo.services.MemberManagementService;
-
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,7 +21,7 @@ public class MemberServiceTest{
 
     @Autowired
     private MemberManagementService mms;
-    private Member mb;
+    private final Member mb;
     public MemberServiceTest() {
         this.mb = new Member("S001", "Janne Björnsson", "0758293153");
     }

67f5a7e69e2b6a04b21679bd057ab13418401bd8
Kod-Draken <m.dackhammar@gmail.com>
2025-05-23 14:24:00 +0200

    added GymClassNotFoundException and implemented it

--- src/main/java/se/yrgo/dataaccess/GymClassDao.java
+++ src/main/java/se/yrgo/dataaccess/GymClassDao.java
@@ -1,5 +1,6 @@
 package se.yrgo.dataaccess;
 
+import se.yrgo.dataaccess.exceptions.GymClassNotFoundException;
 import se.yrgo.domain.GymClass;
 
 import java.util.List;
@@ -9,9 +10,9 @@ import java.util.List;
  */
 public interface GymClassDao {
     public void createGymClass(GymClass gymClass);
-    public void updateGymClass(GymClass gymClass);
-    public void deleteGymClass(GymClass gymClass);
-    public GymClass getGymClassById(String id);
+    public void updateGymClass(GymClass gymClass) throws GymClassNotFoundException;
+    public void deleteGymClass(GymClass gymClass) throws GymClassNotFoundException;
+    public GymClass getGymClassById(String id) throws GymClassNotFoundException;
     public List<GymClass> getGymClassesByName(String name);
     public List<GymClass> getGymClassesByInstructor(String instructorId);
     public List<GymClass> getAllGymClasses();

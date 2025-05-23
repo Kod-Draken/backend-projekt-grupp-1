2bc1ca674c7da10273c6df878fad870adab9d1b9
Kod-Draken <m.dackhammar@gmail.com>
2025-05-23 14:36:23 +0200

    updated gymclassTest according to the new XML file

--- src/test/java/se/yrgo/integrationtests/GymClassServiceTest.java
+++ src/test/java/se/yrgo/integrationtests/GymClassServiceTest.java
@@ -25,7 +25,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @Transactional
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration( {"/other-tiers.xml", "/datasource-test.xml"} )
+@ContextConfiguration( {"/other-tiers-test.xml", "/datasource-test.xml"} )
 public class GymClassServiceTest {
 
     @Autowired

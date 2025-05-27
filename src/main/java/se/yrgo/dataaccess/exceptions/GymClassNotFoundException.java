eb7f169b20243a8515c764fca06966683c1c3cf9
Kod-Draken <m.dackhammar@gmail.com>
2025-05-27 19:44:03 +0200

    final touches

--- src/main/java/se/yrgo/dataaccess/exceptions/GymClassNotFoundException.java
+++ src/main/java/se/yrgo/dataaccess/exceptions/GymClassNotFoundException.java
@@ -1,5 +1,9 @@
 package se.yrgo.dataaccess.exceptions;
 
+/**
+ * @author Mattias
+ * Thrown when a specified gym class cannot be found in the database
+ */
 public class GymClassNotFoundException extends RuntimeException {
     public GymClassNotFoundException(String message) {
         super(message);
--- src/main/java/se/yrgo/services/exceptions/AlreadyBookedToGymClassException.java
+++ src/main/java/se/yrgo/services/exceptions/AlreadyBookedToGymClassException.java
@@ -1,5 +1,9 @@
 package se.yrgo.services.exceptions;
 
+/**
+ * @author Mattias
+ * Thrown when trying to book a member to a class they are already booked to
+ */
 public class AlreadyBookedToGymClassException extends Exception {
     public AlreadyBookedToGymClassException(String message)  {
         super(message);
